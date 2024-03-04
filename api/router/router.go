1f337c5b0625dee0647d46afcab213eb73e735af
Vladislav <vk.azeria@gmail.com>
2024-03-04 21:48:34 +0500

    add an index route

--- api/router/router.go
+++ api/router/router.go
@@ -1,13 +1,15 @@
 package router
 
 import (
+	"music-backend/api/handlers"
+
 	"github.com/julienschmidt/httprouter"
 )
 
 func SetupRouter() *httprouter.Router{
 	r := httprouter.New()
 
-	
+	r.GET("/", handlers.IndexHandler)
 
 	return r
 }
